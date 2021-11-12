5d4421ae47d910f2c6c8696535685ecd2b73c2cb
Jan Koželuh <46165619+Jankozeluh@users.noreply.github.com>
2021-11-12 19:23:54 +0100

    not Reserve Auctions(met not met), dates fixed

--- F1Authentics/webscr.R
+++ F1Authentics/webscr.R
@@ -1,6 +1,9 @@
 library(tidyverse)#full library(tidyverse) maybe useful but not necessary if you want to just get the data use dplyr instead
 library(rvest)
+#library(lubridate)  not actually used, but pretty nice library if you want to do something more with dates
 #library(ggthemes) for some pretty graph templates(themes) - not necessary
+
+lct <- Sys.getlocale("LC_TIME"); Sys.setlocale("LC_TIME", "C")
 
 link <- "https://auction.f1authentics.com/iSynApp/allAuction.action"
 nof <- read_html(link) %>% html_nodes(".result-count") %>% html_text() %>% str_remove_all("\u000A") 
@@ -11,7 +14,7 @@ title <- page %>% html_nodes(".list-item__title a") %>% html_text()
 time <- page %>% html_nodes(".list-item__opened span") %>% html_text() %>% strptime(., "%b %d, %Y %I:%M:%S %p",tz="Europe/London")
 
 price <- page %>% html_nodes(".list-item__curbid strong") %>% html_text() %>% str_remove_all("[£,]") %>% as.numeric()
-n_of_bids <- page %>% html_nodes(".list-item__numbids strong span") %>% html_text() + as.numeric()
+n_of_bids <- page %>% html_nodes(".list-item__numbids strong span") %>% html_text() %>%  as.numeric()
 product_img <- page %>% html_nodes(".list-item__img-container img") %>% html_attr("src")
 link <- page %>% html_nodes(".list-item__title a") %>% html_attr("href") %>% paste("https://auction.f1authentics.com",.,sep="")
 
@@ -22,25 +25,29 @@ get_closer_info <- function(product_link){
   qua <- page %>% html_nodes(".auction-stats__increment+ .auction-stats__qty td") %>% html_text() %>% as.numeric()
   listin <- page %>% html_nodes(".auction-stats__id td") %>% html_text()
   skuu <- page %>% html_nodes(".auction-stats__tag td") %>% html_text()
-  open <- page %>% html_nodes(".auction-stats__tag+ .auction-stats__qty td") %>% html_text() %>% strptime(., "%b %d, %Y %I:%M:%S %p",tz="Europe/London")
+  open <- page %>% html_nodes(".auction-stats__tag+ .auction-stats__qty td") %>% html_text()
   listinT <- page %>% html_nodes(".listing-type-value") %>% html_text()
-  met <- page %>% html_nodes(".mt-xs-3 div") %>% html_elements("span") %>% html_text()
-  descc <- page %>% html_nodes(".sharing--auction-display , p") %>% html_elements("p") %>% html_text() %>% str_remove_all("\u000A") %>%  paste(collapse = " ") 
+  if(listinT == "RESERVE AUCTION"){
+    met <- page %>% html_nodes(".mt-xs-3 div") %>% html_elements("span") %>% html_text()
+  }
+  else{
+    met <- NA
+  }
+  descc <- page %>% html_nodes(".sharing--auction-display , p") %>% html_elements("p") %>% html_text() %>% str_remove_all("\u000A") %>% paste(collapse = " ") 
 
-  return(c(bidinc,qua,listin,skuu,open,listinT,met,descc))
+  return(c(bidinc,qua,listin,skuu,open,listinT,descc,met))
 }
 
 closer_info = sapply(link, FUN = get_closer_info, USE.NAMES = FALSE)
-offered_items <- tibble(Title = title, Desc = closer_info[8,] ,Start = closer_info[5,], End=time, CurrentPrice=price, NBids=n_of_bids, MetNot = closer_info[7,], ProductImg=product_img, Link=link, BidIncrement = closer_info[1,], Quantity = closer_info[2,],  Listing = closer_info[3,], SKU = closer_info[4,], ListingType = closer_info[6,])
-
-write.csv2(offered_items , file = "~/Documents/R/f1/tracks_ex/dd.csv", row.names = FALSE)
-
+offered_items <- tibble(Title = title, Desc = closer_info[7,], Start =  strptime(closer_info[5,], "%b %d, %Y %I:%M:%S %p",tz="Europe/London"), End=time, CurrentPrice=price, NBids=n_of_bids, MetNot = closer_info[8,], ProductImg=product_img, Link=link, BidIncrement = closer_info[1,], Quantity = closer_info[2,],  Listing = closer_info[3,], SKU = closer_info[4,], ListingType = closer_info[6,])
+write.csv2(offered_items , file = "~/Documents/R/f1/tracks_ex/data.csv", row.names = FALSE)
 
 
 #Searching(filtering) by the title
 low <- tolower(offered_items$Title)
 offered_items %>% filter(grepl("alonso", low))
 #
+
 
 #Simple graph example(actually not interesting)
 offered_items %>% 
