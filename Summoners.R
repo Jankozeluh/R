9393da441428df247ff5fceea3a6b6b913527503
Jankozeluh <honzik.kozeluh@seznam.cz>
2022-02-19 20:03:44 +0100

    little bit of work, separating files, adding little more complexity to the summonerInfo function

--- Summoners.R
+++ Summoners.R
@@ -1,66 +1,97 @@
-Packages <- c("tidyverse", "httr", "jsonlite")
+Packages <- c("tidyverse", "httr", "jsonlite", "lubridate")
 lapply(Packages, library, character.only = TRUE)
+options("scipen"=100, "digits"=4)
 
-api_key <- "RGAPI-XXXXX"
+api_key <- "RGAPI-XXXX"
+
+name <- "Agurin"
+region <- "euw1"
+m_url <- paste0("https://", region, ".api.riotgames.com/lol/summoner/v4/summoners/by-name/", name, "?api_key=", api_key, sep = "", collapse = "")
+
+m_content <- httr::GET(m_url) %>%
+  httr::content(., as = 'text') %>%
+  jsonlite::fromJSON(.)
+
+print(m_content$puuid)
+
+americas <- c("br1", "na1", "la1", "la2", "oc1")
+europe <- c("euw1", "eune1")
+asia <- c("kr", "tr1", "jp1", "ru")
+
+if (region %in% americas) {
+  region <- "americas"
+}else if (region %in% europe) {
+  region <- "europe"
+}else if (region %in% asia) {
+  region <- "asia"
+}else {
+  return("BG, wrong region.")
+}
+
+start <- 0
+count <- 20
+
+match_history <- paste0("https://", region, ".api.riotgames.com/lol/match/v5/matches/by-puuid/", m_content$puuid, "/ids?start=", start, "&count=", count, "&api_key=", api_key) %>%
+  httr::GET(.) %>%
+  httr::content(.$id, as = 'text') %>%
+  jsonlite::fromJSON(.)
+
+closer <- function (match_id){
+  link <- paste0("https://", region, ".api.riotgames.com/lol/match/v5/matches/", match_id, "?api_key=", api_key, sep = "", collapse = "") %>%
+    httr::GET(.) %>%
+    httr::content(., as = 'text') %>%
+    jsonlite::fromJSON(.)
+  return(link)
+}
+
+closer_match_history_info <- lapply(match_history,closer)
+
+#gameEnd(Creation) - milliseconds
+#gameDuration - seconds
+
+end_time <- c[[1]]$info$gameCreation/1000
+end_time
+seconds_to_period(end_time)
+as.POSIXct(end_time, origin="1970-01-01")
 
 summonerInfo <- function(name, region) {
   m_url <- paste0("https://", region, ".api.riotgames.com/lol/summoner/v4/summoners/by-name/", name, "?api_key=", api_key, sep = "", collapse = "")
-  m_content <- httr::GET(m_url) %>%
-    httr::content(r_result, as = 'text') %>%
+
+  summoner <- httr::GET(m_url) %>%
+    httr::content(., as = 'text') %>%
     jsonlite::fromJSON(.)
-  dplyr::glimpse(m_content)
-}
 
-summonerMatches <- function(name, region, start, count) {
-  if (start < count) {
-    m_url <- paste0("https://", region, ".api.riotgames.com/lol/summoner/v4/summoners/by-name/", name, "?api_key=", api_key, sep = "", collapse = "")
-    puuid <- httr::GET(m_url) %>%
-      httr::content(r_result, as = 'text') %>%
-      jsonlite::fromJSON(.) %>%
-      .$puuid
+  americas <- c("br1", "na1", "la1", "la2", "oc1")
+  europe <- c("euw1", "eune1")
+  asia <- c("kr", "tr1", "jp1", "ru")
 
-    americas <- c("br1", "na1", "la1", "la2", "oc1")
-    europe <- c("euw1", "eune1")
-    asia <- c("kr", "tr1", "jp1", "ru")
+  if (region %in% americas) {
+    region <- "americas"
+  }else if (region %in% europe) {
+    region <- "europe"
+  }else if (region %in% asia) {
+    region <- "asia"
+  }else {
+    return("BG, wrong region.")
+  }
 
-    if (region %in% americas) {
-      region <- "americas"
-    }else if (region %in% europe) {
-      region <- "europe"
-    }else if (region %in% asia) {
-      region <- "asia"
-    }else {
-      return("BG, wrong region.")
-    }
+  match_history <- paste0("https://", region, ".api.riotgames.com/lol/match/v5/matches/by-puuid/", m_content$puuid, "/ids?start=", start, "&count=", count, "&api_key=", api_key) %>%
+    httr::GET(.) %>%
+    httr::content(.$id, as = 'text') %>%
+    jsonlite::fromJSON(.)
 
-    m_url <- paste0("https://", region, ".api.riotgames.com/lol/match/v5/matches/by-puuid/", puuid, "/ids?start=", start, "&count=", count, "&api_key=", api_key, sep = "", collapse = "")
-
-    m_content <- httr::GET(m_url) %>%
-      httr::content(r_result, as = 'text') %>%
+  closer <- function (match_id){
+    link <- paste0("https://", region, ".api.riotgames.com/lol/match/v5/matches/", match_id, "?api_key=", api_key, sep = "", collapse = "") %>%
+      httr::GET(.) %>%
+      httr::content(., as = 'text') %>%
       jsonlite::fromJSON(.)
-
-    dplyr::glimpse(m_content)
+    return(link)
   }
-  else {
-    print("start match-id must be lower than end")
-  }
-}
 
-closer_match_info <- function (match_id, region, timeline){
-  if(region != "europe" || "americas" || "asia"){
-  m_url <- paste0("https://", region, ".api.riotgames.com/lol/match/v5/matches/", match_id, if(timeline) "/timeline", "?api_key=", api_key, sep = "", collapse = "")
+  closer_match_history_info <- lapply(match_history,closer)
 
-  m_content <- httr::GET(m_url) %>%
-    httr::content(r_result, as = 'text') %>%
-    jsonlite::fromJSON(.)
 
-  # print(m_url)
-  dplyr::glimpse(m_content)
-  }else{
-    print("BG, wrong region(here are only three - europe, asia, americas)")
-  }
+  return(closer_match_history_info) #not gonna work like this at the end
 }
 
 summonerInfo('Agurin', 'euw1')
-summonerMatches('Agurin', 'euw1', 0, 20)
-closer_match_info("EUW1_5729711315", "europe", FALSE)
